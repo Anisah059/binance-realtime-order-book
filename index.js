63739f79bc463e7bc7b7ce9852c25c7f33435869
Anisah <123579875+Anisah059@users.noreply.github.com>
2023-02-20 18:37:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
 
-// const OrderBook = require('./lib/orderBook');
-// const SocketClient = require('./lib/socketClient');
-// const { orderbookUpdateFromWebsocket, orderBookUpdateFromRESTfulAPI } = require('./lib/orderBookManager');
+const OrderBook = require('./lib/orderBook');
+const SocketClient = require('./lib/socketClient');
+const { orderbookUpdateFromWebsocket, orderBookUpdateFromRESTfulAPI } = require('./lib/orderBookManager');
 
 const SYMBOL = process.env.SYMBOL || 'BTCUSDT';
 
-// const socketApi = new SocketClient(`ws/${SYMBOL.toLowerCase()}@depth`);
+const socketApi = new SocketClient(`ws/${SYMBOL.toLowerCase()}@depth`);
 
-// const orderBook = new OrderBook(SYMBOL.toUpperCase());
+const orderBook = new OrderBook(SYMBOL.toUpperCase());
 
-// socketApi.setHandler('depthUpdate', (params) => orderbookUpdateFromWebsocket(params)(orderBook));
+socketApi.setHandler('depthUpdate', (params) => orderbookUpdateFromWebsocket(params)(orderBook));
 
-// // leave a time gap to wait for websocket connection first
-// setTimeout(() => {
-//   orderBookUpdateFromRESTfulAPI(orderBook);
-// }, 3000);
-
-// const fetchOrderBook = () => {
-//   console.log('A');
-//   return orderBook.inspect();
-// }
-
-// module.exports = {
-//   fetchOrderBook
-// };
-
-const Binance = require('node-binance-api');
-const binance = new Binance();
-
-binance.depth("BTCUSDT", (error, depth, symbol) => {
-  console.info(symbol+" market depth", depth);
-});
+// leave a time gap to wait for websocket connection first
+setTimeout(() => {
+  orderBookUpdateFromRESTfulAPI(orderBook);
+}, 3000);
 
 const fetchOrderBook = () => {
-  return new Promise( (resolve, reject) => {
-    binance.depth(SYMBOL, (error, depth, symbol) => {
-      // console.info(symbol+" market depth", depth);
-      resolve(depth);
-    });
-  });
+  console.log('A');
+  return orderBook.inspect();
 }
 
 module.exports = {
