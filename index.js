cdfb06336f1f9054c62c1333896e5dd2d6782107
Anisah <123579875+Anisah059@users.noreply.github.com>
2023-02-20 18:31:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+
+// const OrderBook = require('./lib/orderBook');
+// const SocketClient = require('./lib/socketClient');
+// const { orderbookUpdateFromWebsocket, orderBookUpdateFromRESTfulAPI } = require('./lib/orderBookManager');
+
+const SYMBOL = process.env.SYMBOL || 'BTCUSDT';
+
+// const socketApi = new SocketClient(`ws/${SYMBOL.toLowerCase()}@depth`);
+
+// const orderBook = new OrderBook(SYMBOL.toUpperCase());
+
+// socketApi.setHandler('depthUpdate', (params) => orderbookUpdateFromWebsocket(params)(orderBook));
+
+// // leave a time gap to wait for websocket connection first
+// setTimeout(() => {
+//   orderBookUpdateFromRESTfulAPI(orderBook);
+// }, 3000);
+
+// const fetchOrderBook = () => {
+//   console.log('A');
+//   return orderBook.inspect();
+// }
+
+// module.exports = {
+//   fetchOrderBook
+// };
+
+const Binance = require('node-binance-api');
+const binance = new Binance();
+
+binance.depth("BTCUSDT", (error, depth, symbol) => {
+  console.info(symbol+" market depth", depth);
+});
+
+const fetchOrderBook = () => {
+  return new Promise( (resolve, reject) => {
+    binance.depth(SYMBOL, (error, depth, symbol) => {
+      // console.info(symbol+" market depth", depth);
+      resolve(depth);
+    });
+  });
+}
+
+module.exports = {
+  fetchOrderBook
+};
